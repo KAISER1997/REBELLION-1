9a7a24c536b26eee959c81442f8cf5ddde63d44a
KAISER1997 <kingkenshin1997@gmail.com>
2017-12-15 20:30:59 +0530

    codeforces

---
+++ 103A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+int n;
+cin>>n;
+long long int toral=0,z;
+for(int e=1;e<=n;e++)
+{
+cin>>z;
+toral+=e*(z-1)+1;
+
+
+
+
+}
+
+
+cout<<toral;
+
+
+
+
+
+
+}
