a3366eda431039e86274b811fe1375b38f30294d
KAISER1997 <kingkenshin1997@gmail.com>
2018-03-14 03:03:42 +0530

    uva

---
+++ 10678.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+	int h;
+	cin>>h;
+	for(int u=1;u<=h;u++)
+	{
+double pi=acos(0)*2;
+double z2ae,z2a,a;
+cin>>z2ae>>z2a;
+a=z2a/2;
+double e=z2ae/z2a;
+double b=a*sqrt(1-(e*e));
+ std::cout << std::fixed;
+    std::cout << std::setprecision(3);
+cout<<a*b*pi<<endl;
+
+}
+
+
+
+}
